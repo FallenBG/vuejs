f57ae878433ad2ae123d8db4db944e009d617152
mdaskalov <thefallenangel.bg@gmail.com>
2019-08-02 14:57:32 +0800

    Lesson16

---
+++ src/components/Message.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="box">
+        <p>
+            {{ msg }}
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Message',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .box {
+        background: antiquewhite;
+        padding: 10px;
+        border: 1px solid #42b983;
+    }
+</style>
